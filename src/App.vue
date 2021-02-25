71de99ba9d6f8b92808124513d4ce3b1104b3142
LCStudio-Chad <chad@lcstudio.co.za>
2021-02-25 16:32:43 +0200

    layout init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,136 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="home" class="row">
+    <div id="side-nav">
+      <div id="logo" class="row">
+        <div class="nav-logo">
+          <!-- <img src="" /> -->
+          <div id="logo-cntr">
+            <span style="font-size: 50px; color: white; font-weight: bold">C</span>
+            <p style="margin-top: -15px; color: white;">CBAZIL</p>
+          </div>
+        </div>
+      </div>
+      <div id="navigation" class="row">
+        <div class="nav-i">
+          <q-icon
+            size="30px"
+            name="fas fa-shapes"
+            color="white"
+          />
+        </div>
+        <div class="nav-i">
+          <q-icon
+            size="30px"
+            name="fas fa-user-tie"
+            color="white"
+          />
+        </div>
+        <div class="nav-i">
+          <q-icon
+            size="30px"
+            name="fas fa-cubes"
+            color="white"
+          />
+        </div>
+        <div class="nav-i">
+          <q-icon
+            size="30px"
+            name="fas fa-book-reader"
+            color="white"
+          />
+        </div>
+        <div class="nav-i">
+          <q-icon
+            size="30px"
+            name="fas fa-layer-group"
+            color="white"
+          />
+        </div>
+      </div>
+      <div id="s-links" class="row">
+        <div class="link-i">
+          <q-icon
+            size="xs"
+            name="fab fa-linkedin"
+            color="white"
+          />
+        </div>
+        <div class="link-i">
+          <q-icon
+            size="xs"
+            name="fab fa-twitter"
+            color="white"
+          />
+        </div>
+        <div class="link-i">
+          <q-icon
+            size="xs"
+            name="fab fa-github"
+            color="white"
+          />
+        </div>
+        <div class="link-i">
+          <q-icon
+            size="xs"
+            name="fab fa-medium-m"
+            color="white"
+          />
+        </div>
+      </div>
+    </div>
+    <div id="main-container" class="col"></div>
   </div>
-  <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { ref } from 'vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'LayoutDefault',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    // HelloWorld
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  setup () {
+    return {
+      // leftDrawerOpen: ref(false)
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+$dark: #2b2c2e;
+$grey: #353639;
+
+#home {
+  background-color: $grey;
+  width: 100wh;
+  height: 100vh; 
+
+  #side-nav {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    height: inherit;
+    background-color: $dark;
+    justify-content: space-between;
+    width: 70px;
+  }
+
+  #main-container {
+    // background-color: khaki;
+  }
+
+  .nav-logo, .nav-i, .link-i {
+    text-align: center;
+    padding: 10px 0;
+    width: 100%;
+    display: block;
+    cursor: pointer;
+  }
+
 }
 </style>
