023400db5ce3fb88f2a43e43a9b3711f8a262f47
LCStudio-Chad <chad@lcstudio.co.za>
2021-03-04 16:30:44 +0200

    binding styles

--- src/App.vue
+++ src/App.vue
@@ -10,54 +10,60 @@
         </div>
       </div>
       <div id="navigation" class="row">
-        <div class="nav-i">
+        <div class="nav-i" :class="{on_page: on_home}">
           <router-link to="/">
             <q-icon
+              class="icons"
               size="30px"
               name="fas fa-shapes"
               color="white"
             />
           </router-link>
         </div>
-        <div class="nav-i">
+        <div class="nav-i" :class="{on_page: on_about}">
           <router-link to="/about">
             <q-icon
+              class="icons"
               size="30px"
               name="fas fa-user-tie"
               color="white"
             />
           </router-link>
         </div>
-        <div class="nav-i">
+        <div class="nav-i" :class="{on_page: on_skills}">
           <router-link to="/skills">
             <q-icon
+              class="icons"
               size="30px"
               name="fas fa-cubes"
               color="white"
             />
           </router-link>
         </div>
-        <div class="nav-i">
+        <div class="nav-i" :class="{on_page: on_exp}">
           <router-link to="/experience">
             <q-icon
+              class="icons"
               size="30px"
               name="fas fa-book-reader"
               color="white"
             />
           </router-link>
         </div>
-        <div class="nav-i">
+        <div class="nav-i" :class="{on_page: on_blogs}">
           <router-link to="/blogs">
             <q-icon
+              class="icons"
               size="30px"
               name="fas fa-newspaper"
               color="white"
             />
           </router-link>
         </div>
-        <div class="nav-i">
+        <div class="nav-i" :class="{on_page: on_projects}">
           <router-link to="/projects">
             <q-icon
+              class="icons"
               size="30px"
               name="fas fa-layer-group"
               color="white"
@@ -107,16 +113,20 @@ import { ref } from 'vue'
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'LayoutDefault',
+  name: 'Landing Page',
 
   components: {
     // HelloWorld
   },
+  setup() {
+   let on_home = false;
+   let on_about = false;
+   let on_skills = false;
+   let on_exp = false;
+   let on_blogs = false;
+   let on_projects = false;
 
-  setup () {
-    return {
-      // leftDrawerOpen: ref(false)
-    }
+   return { on_home, on_about, on_skills, on_exp, on_blogs, on_projects };
   }
 }
 </script>
@@ -124,11 +134,13 @@ export default {
 
 $dark: #2b2c2e;
 $grey: #353639;
+$green: #7ed957;
+$blue: #5271ff;
 
 #home {
   background-color: $grey;
   width: 100wh;
-  height: 100vh; 
+  height: 100vh;
 
   #side-nav {
     position: relative;
@@ -139,7 +151,6 @@ $grey: #353639;
     justify-content: space-between;
     width: 80px;
   }
-
   #logo-cntr {
     width: inherit;
     height: auto;
@@ -149,18 +160,25 @@ $grey: #353639;
       height: inherit;
     }
   }
-
   #main-container {
-    // background-color: khaki;
+    // background-color: khaki
   }
+  
   #s-links {
     margin-bottom: 20px;
   }
+  
   .nav-logo, .nav-i, .link-i {
     text-align: center;
     width: 100%;
     display: block;
     cursor: pointer;
+  }
+  .on_page {
+    background: $grey;
+    .icons {
+      color: $green !important;
+    }
   }
   .link-i {
       padding: 10px 0;
@@ -168,6 +186,5 @@ $grey: #353639;
   .nav-i {
       padding: 20px 0;
   }
-
 }
 </style>
